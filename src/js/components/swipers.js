bb340af1f84e3133b5309155169c31f7cb902d7b
Eneyl <arqq.au@gmail.com>
2021-05-05 20:07:32 +0300

    initial commit

--- src/js/components/swipers.js
+++ src/js/components/swipers.js
@@ -23,6 +23,44 @@ new Swiper('.differ__inner-container', {
   navigation: {
     nextEl: '.differ-swiper-buttons__next',
     prevEl: '.differ-swiper-buttons__prev',
-    disabledClass: 'differ-swiper-buttons--disabled'
+    disabledClass: 'swiper-buttons--disabled'
+  },
+});
+
+//Case swiper
+new Swiper('.case__container', {
+  slidesPerView: 1,
+  spaceBetween: 50,
+  pagination: {
+    el: ".case-swiper-buttons__pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: '.case-swiper-buttons__next',
+    prevEl: '.case-swiper-buttons__prev',
+    disabledClass: 'swiper-buttons--disabled'
+  },
+});
+
+new Swiper('.team__inner-container', {
+  effect: "coverflow",
+  grabCursor: true,
+  centeredSlides: true,
+  slidesPerView: "auto",
+  coverflowEffect: {
+    rotate: 50,
+    stretch: 10,
+    depth: 200,
+    modifier: 1,
+    slideShadows: false,
+  },
+  pagination: {
+    el: ".team-swiper-buttons__pagination",
+    type: "fraction",
+  },
+  navigation: {
+    nextEl: '.team-swiper-buttons__next',
+    prevEl: '.team-swiper-buttons__prev',
+    disabledClass: 'swiper-buttons--disabled'
   },
 });
